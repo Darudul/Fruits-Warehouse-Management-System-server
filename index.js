adc58b2b9ad1c480a22bda26d56df5d25d903a67
Darudul <darudulislam@gmail.com>
2022-05-09 00:23:20 +0800

    added JWT authentication

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ app.use(express.json());
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
-function verifyJWT(req, res, next) {
+function AuthorizatonJWt(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res.status(401).send({ message: "unauthorized access" });
+    return res.status(401).send({ message: "Unauthorized Access" });
   }
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(403).send({ message: "Forbidden access" });
+      return res.status(403).send({ message: "Forbidden Access" });
     }
     console.log("decoded", decoded);
     req.decoded = decoded;
@@ -35,7 +35,6 @@ async function run() {
   try {
     await client.connect();
     const fruitsCollection = client.db("fruits1").collection("products");
-    console.log("rtgghh");
     app.post("/login", async (req, res) => {
       const user = req.body;
       console.log(user);
@@ -91,35 +90,27 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/myItem", verifyJWT, async (req, res) => {
+    app.get("/myItem", AuthorizatonJWt, async (req, res) => {
       const decodedEmail = req.decoded.email;
       const email = req.query.email;
-      // console.log(email);
       if (email === decodedEmail) {
         const query = { email: email };
         const cursor = fruitsCollection.find(query);
         const orders = await cursor.toArray();
         res.send(orders);
       } else {
-        res.status(403).send({ message: "forbidden access" });
+        res.status(403).send({ message: "Forbidden access" });
       }
     });
-    // app.delete("/myItem/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   console.log(id);
-    //   const query = { _id: ObjectId(id) };
-    //   const result = await fruitsCollection.deleteOne(query);
-    //   res.send(result);
-    // });
   } finally {
   }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("fruits server");
+  res.send("fruits server warehouse");
 });
 
 app.listen(port, () => {
-  console.log("Ex port", port);
+  console.log("My warehouse management system", port);
 });
