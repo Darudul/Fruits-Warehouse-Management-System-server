dd7100a1033047906e8540d60e0b9100e86cfbba
Darudul <darudulislam@gmail.com>
2022-05-08 14:28:12 +0800

    added token

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
 require("dotenv").config();
-
 app.use(cors());
 app.use(express.json());
+const jwt = require("jsonwebtoken");
+const port = process.env.PORT || 5000;
+
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+    next();
+  });
+}
 
 const uri = `mongodb+srv://${process.env.DB_Fruits}:${process.env.DB_PASS}@cluster0.g0slb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -19,7 +35,16 @@ async function run() {
   try {
     await client.connect();
     const fruitsCollection = client.db("fruits1").collection("products");
-    const orderCollection = client.db("fruits1").collection("order");
+    console.log("rtgghh");
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+
     app.get("/fruit", async (req, res) => {
       const query = {};
       const cursor = fruitsCollection.find(query);
@@ -66,13 +91,18 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/myItem", async (req, res) => {
+    app.get("/myItem", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const email = req.query.email;
       // console.log(email);
-      const query = { email: email };
-      const cursor = fruitsCollection.find(query);
-      const orders = await cursor.toArray();
-      res.send(orders);
+      if (email === decodedEmail) {
+        const query = { email: email };
+        const cursor = fruitsCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+      } else {
+        res.status(403).send({ message: "forbidden access" });
+      }
     });
     // app.delete("/myItem/:id", async (req, res) => {
     //   const id = req.params.id;
